317cecfbf79da6badb31db8c4d136c8b61a2f807
Otar989 <otar989@gmail.com>
2025-08-12 19:00:27 +0300

    Исправлен запуск сервера для Render

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log('Server started');
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.end('Server is running');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
